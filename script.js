892bf2d58fb704e888d67a2d69371956112fcad3
Macedo <gm18242@gmail.com>
2024-06-17 17:37:49 -0300

    Criação de checkbox para selecionar informação especifica.

--- script.js
+++ script.js
@@ -14,9 +14,25 @@ const fetchApi = (value) =>{
      return result
 }
 
+const keys = ['name', 'status', 'species', 'gender', 'origin', 'image', 'episode']
+
+const buildResult = (result) =>{
+     const newObject = {}
+     keys.map((key)=> document.getElementById(key))
+     .map((elem)=>{
+          elem.checked && (newObject[elem.name] = result[elem.name])
+     })
+
+     return newObject;
+}
+
+
+
 botaoGo.addEventListener('click', async (event) =>{
      event.preventDefault()
      const result = await fetchApi(caracterId.value)
-     content.textContent = `${JSON.stringify(result, undefined, 2)}`;
+     // content.textContent = `${JSON.stringify(result, undefined, 2)}`;
+     content.textContent = `${JSON.stringify(buildResult(result), undefined, 2)}`;
+     console.log(buildResult(result))
      image.src = `${result.image}`;
 })
