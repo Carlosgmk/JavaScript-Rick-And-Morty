ed3fa77401f63ffbb6ca93564fe673b71b0cb986
Macedo <gm18242@gmail.com>
2024-06-18 22:03:58 -0300

    Mecanismo de busca Atualizado, exibição de elementos estilizados

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
-const caracterId = document.getElementById('caracterId')
+const characterId = document.getElementById('caracterId')
 const botaoGo = document.getElementById('btn-go')
 const content = document.getElementById('content')
 const image = document.getElementById('img')
+const containerResult = document.getElementById('result-style')
+const reset = document.getElementById('reset')
 
 const fetchApi = (value) =>{
      const result = fetch(`https://rickandmortyapi.com/api/character/${value}`)
@@ -14,25 +16,61 @@ const fetchApi = (value) =>{
      return result
 }
 
-const keys = ['name', 'status', 'species', 'gender', 'origin', 'image', 'episode']
+const keys = ['name', 'status', 'species', 'gender', 'origin', 'episode']
+const newKeys = {
+     name: 'Nome',
+     status: 'Status',
+     species: 'Espécie',
+     gender: 'Gênero',
+     origin: 'Planeta',
+     episode: 'Episódios'
 
-const buildResult = (result) =>{
-     const newObject = {}
-     keys.map((key)=> document.getElementById(key))
-     .map((elem)=>{
-          elem.checked && (newObject[elem.name] = result[elem.name])
-     })
-
-     return newObject;
 }
 
+const buildResult = (result) =>{
+     return keys.map((key)=> document.getElementById(key))
+     .map((elem)=>{
+          if(elem.checked === true && (Array.isArray(result[elem.name])) == true){
+               const ArrayResult = result[elem.name].join('\r\n')
+               console.log(ArrayResult)
+               const newElement = document.createElement('p')
+               newElement.innerHTML = `${newKeys[elem.name]}: ${ArrayResult}`
+               content.appendChild(newElement)
+
+       }else if(elem.checked === true && (elem.name === 'origin')){
+          const newElement = document.createElement('p')
+          newElement.innerHTML = `${newKeys[elem.name]}: ${result[elem.name].name}`
+          content.appendChild(newElement)
+
+      }else if(elem.checked === true && typeof(result[elem.name]) !== 'object'){
+               const newElement = document.createElement('p')
+               newElement.innerHTML = `${newKeys[elem.name]}: ${result[elem.name]}`
+               content.appendChild(newElement)
+       }
+     })
+}
 
 
 botaoGo.addEventListener('click', async (event) =>{
      event.preventDefault()
-     const result = await fetchApi(caracterId.value)
-     // content.textContent = `${JSON.stringify(result, undefined, 2)}`;
-     content.textContent = `${JSON.stringify(buildResult(result), undefined, 2)}`;
-     console.log(buildResult(result))
-     image.src = `${result.image}`;
+
+     if(characterId.value === ''){
+          return content.innerHTML= 'É necessário fazer um filtro.'
+     }
+
+     const result = await fetchApi(characterId.value)
+     if(content.firstChild === null){
+          containerResult.className = 'result-style' 
+          image.src = `${result.image}`;
+          buildResult(result)
+     } else {
+          content.innerHTML = ''
+          containerResult.className = 'result-style' 
+          image.src = `${result.image}`;
+          buildResult(result)
+     }
+
+    
 })
+
+reset.addEventListener('click', () => location.reload())
